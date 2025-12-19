c7918db6d19faca34f3d9a8548cef7c20ff610fd
glieseKep <starneko89@gmail.com>
2025-12-19 12:55:43 -0500

    Creación de la tabla para la migración a la bd

---
+++ database/migrations/2025_12_19_173003_create_equipos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('tipo', 10);
+            $table->string('marca', 25);
+            $table->text('defecto');
+            $table->string('nombre_duenio', 35);
+            $table->string('telefono_duenio', 10);
+            $table->string('estado', 20);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipos');
+    }
+};
