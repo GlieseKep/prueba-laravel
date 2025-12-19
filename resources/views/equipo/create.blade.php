b619c028ed81fe03743f51a79cdda5fa89cb446e
glieseKep <starneko89@gmail.com>
2025-12-19 13:45:07 -0500

    Cambio del name de los input del form

--- resources/views/equipo/create.blade.php
+++ resources/views/equipo/create.blade.php
@@ -16,28 +16,28 @@
         <!-- Campo de marca -->
         <div class="mb-3">
             <label class="form-label">Marca *</label>
-            <input type="text" name="placa" class="form-control" required maxlength="25">
+            <input type="marca" name="placa" class="form-control" required maxlength="25">
         </div>
         <!-- Campo de defecto -->
         <div class="mb-3">
             <label class="form-label">Defecto</label>
-            <textarea name="observaciones" class="form-control"
+            <textarea name="defecto" class="form-control"
                       rows="3"></textarea>
         </div>
         <!-- Campo de nombre_duenio -->
         <div class="mb-3">
             <label class="form-label">Nombre del dueño *</label>
-            <input type="text" name="placa" class="form-control" required maxlength="50">
+            <input type="text" name="nombre_duenio" class="form-control" required maxlength="50">
         </div>
         <!-- Campo de telefono_duenio -->
         <div class="mb-3">
             <label class="form-label">Teléfono del dueño *</label>
-            <input type="text" name="placa" class="form-control" required maxlength="10">
+            <input type="number" name="telefono_duenio" class="form-control" required maxlength="10">
         </div>
         <!-- Campo de estado -->
         <div class="mb-3">
             <label class="form-label">Estado *</label>
-            <select name="tipo" class="form-select" required>
+            <select name="estado" class="form-select" required>
                 <option value="">Seleccione...</option>
                 <option value="Revisado">Revisado</option>
                 <option value="En reparación">En reparación</option>
