835a96fab6a90f3386a849d9131e3610977e96f7
glieseKep <starneko89@gmail.com>
2025-12-19 13:59:39 -0500

    Creación de la blade para editar equipo

---
+++ resources/views/equipo/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+@section('titulo', 'Editar Vehículo')
+@section('contenido')
+    <h1>Editar Vehículo</h1>
+    <form action="{{ route('vehiculos.update', $equipo) }}" method="POST">
+        @csrf
+        @method('PUT')
+        @csrf
+        <!-- Campo de tipo -->
+        <div class="mb-3">
+            <label class="form-label">Tipo *</label>
+            <select name="tipo" class="form-select" required>
+                <option value="Teléfono" {{ $equipo->tipo == 'Teléfono' ? 'selected' : ''}}>Teléfono</option>
+                <option value="Tablet" {{ $equipo->tipo == 'Tablet' ? 'selected' : ''}}>Tablet</option>
+            </select>
+        </div>
+        <!-- Campo de marca -->
+        <div class="mb-3">
+            <label class="form-label">Marca *</label>
+            <input type="text" name="marca" class="form-control" required maxlength="25" value="{{ $equipo->marca}}">
+        </div>
+        <!-- Campo de defecto -->
+        <div class="mb-3">
+            <label class="form-label">Defecto</label>
+            <textarea name="defecto" class="form-control" rows="3">{{ $equipo->defecto}}</textarea>
+        </div>
+        <!-- Campo de nombre_duenio -->
+        <div class="mb-3">
+            <label class="form-label">Nombre del dueño *</label>
+            <input type="text" name="nombre_duenio" class="form-control" required maxlength="50" value="{{ $equipo->nombre_duenio}}">
+        </div>
+        <!-- Campo de telefono_duenio -->
+        <div class="mb-3">
+            <label class="form-label">Teléfono del dueño *</label>
+            <input type="number" name="telefono_duenio" class="form-control" required maxlength="10" value="{{ $equipo->telefono_duenio}}">
+        </div>
+        <!-- Campo de estado -->
+        <div class="mb-3">
+            <label class="form-label">Estado *</label>
+            <select name="estado" class="form-select" required>
+                <option value="Revisado" {{ $equipo->estado == 'Revisado' ? 'selected' : ''}}>Revisado</option>
+                <option value="En reparación" {{ $equipo->estado == 'En reparación' ? 'selected' : ''}}>En reparación</option>
+                <option value="Reparado" {{ $equipo->estado == 'Reparado' ? 'selected' : ''}}>Reparado</option>
+                <option value="No necesitó arreglo" {{ $equipo->estado == 'No necesritó arreglo' ? 'selected' : ''}}>No necesitó arreglo</option>
+            </select>
+        </div>
+        </div>
+        <a href="{{ route('equipo.index') }}" class="btn btn-secondary">Cancelar</a>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
+    </form>
+@endsection
